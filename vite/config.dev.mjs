e1aa8d29a22277c1f0b4661b924ccfad039547c6
John-Paul Newton <john-paulnewton@johnpaulsair9.mynetworksettings.com>
2026-04-23 00:01:43 -0400

    revert deployment fix attempt

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/ud-s26-cisc374-final/',
+    base: "./",
     plugins: [react()],
     server: {
         port: 8080,
